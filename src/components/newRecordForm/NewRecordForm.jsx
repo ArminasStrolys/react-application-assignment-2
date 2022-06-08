be544aa7e723280112a1d65faab4bdef875b7d1f
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-06-08 10:12:27 +0300

    Creating POST method

--- src/components/newRecordForm/NewRecordForm.jsx
+++ src/components/newRecordForm/NewRecordForm.jsx
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 const NewRecordForm = () => {
-  const [newPost, setNewPost] = useState([]);
+  const [newPost, setNewPost] = useState();
+  const [visible, setVisible] = useState(true);
+  const [userPost, setUserPost] = useState({
+    body:'',
+    title:'',
+    userId:''
+  })
+
+  const handleVisibility = (e) => {
+    e.preventDefault();
+    visible === true ? setVisible(false) : setVisible(true);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target.value)
+  }
 
   useEffect(() => {
     const createPost = {
-      method: "POST",
-      body: "Lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem",
-      id: 198,
-      title: "Lorem testum",
-      userId: 111,
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({  userId: userPost.userId, title: userPost.title, body: userPost.body })
     };
-
-    fetch(`https://jsonplaceholder.typicode.com/posts`, createPost)
-      .then((res) => res.json())
-      .then((data) => setNewPost(data))
-      .catch((error) => console.log(error));
-  }, []);
+    fetch('https://jsonplaceholder.typicode.com/posts', createPost)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}, []);
 
   console.log(newPost);
 
@@ -28,7 +39,7 @@ const NewRecordForm = () => {
         </button>
       </form>
       <>
-        <form className="detail" to="/details">
+        <form onSubmit={(e) => e.preventDefault()} className="new-form-page">
           <div className="post">
             <table className="table">
               <tbody>
@@ -41,20 +52,55 @@ const NewRecordForm = () => {
                 <tr>
                   <td>
                     <label>Context: </label>
-                    {/* <input className="context" type="text" placeholder="Enter context here" /> */}
-                    <textarea id="w3review" name="w3review" rows="4" cols="50" placeholder="Enter context here"></textarea>
+                    <textarea
+                      id="w3review"
+                      name="w3review"
+                      rows="4"
+                      cols="50"
+                      placeholder="Enter context here"
+                    ></textarea>
                   </td>
                 </tr>
                 <tr>
                   <td>
                     <label>User name: </label>
-                    <input type="text" placeholder="Enter your name here" />
+                    <input type="text" placeholder="Enter your name here" onChange={handleChange}/>
                   </td>
                 </tr>
               </tbody>
             </table>
+            <button className="form-submit-btn" type="submit">
+              Post
+            </button>
           </div>
         </form>
+        <div
+          style={{ display: visible === true ? "none" : "block" }}
+          className="post-pop-up"
+        >
+          <form onSubmit={handleVisibility}>
+            <button type="submit" className="close-window">
+              X
+            </button>
+          </form>
+          <div>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    <b>TITLE</b>
+                  </td>
+                </tr>
+                <tr>
+                  <td>BODY</td>
+                </tr>
+                <tr>
+                  <td>USER</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </>
     </div>
   );
